2521618242ae307e5376ceeb8506999d9723afa6
Ollie <olliehd1322@gmail.com>
2020-11-24 07:13:26 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function operate (a,operator,b){
+    //addition
+    if (operator == "+"){
+        add(a,b);
+    } 
+    //subtraction
+    else if (operator == "-") {
+        subtract(a,b);
+    }
+    //multiplication
+    else if (operator == "*"){
+        return multiply(a,b)
+    }
+    //division
+    else if (operator == "/"){
+        divide(a,b);
+    }
+}
+
+function add(a,b){
+    return (a + b);
+}
+
+function subtract(a,b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b)
+}
+
+function divide(a,b){
+    return (a / b);
+}
