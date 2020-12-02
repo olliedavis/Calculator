ae35340a8ca3210e7919e4f8694ce89627b9b051
Ollie <olliehd1322@gmail.com>
2020-12-02 19:29:04 +0000

    Final Version

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let display = document.getElementById("number-display");
-let inputVal = 0;
+let inputVal;
 let valueArr = [];
 let operatorChoice;
 let total = 0;
 let currentArr = [];
 let sum1 = 0;
 let sum2 = 0;
-let lightSwitch=0
+let lightSwitch = 0;
 
 //Reloads the page, resetting all variables in the process
 function clearDisplay(){
@@ -15,8 +15,9 @@ function clearDisplay(){
 
 //takes the value of the function and pushes it to the array, displaying it in the process. When pressed again, it gets added to the array and then joined together to make one number. 
 function sum(value){
-    if(value == "." && currentArr.includes(".")){
-        value = "";
+    //prevents multiple decimal points & prevents adding digits directly after calculation without an operator first
+    if((value == "." && currentArr.includes(".")) || (total && operatorChoice == "")){
+        return;
         }
     currentArr.push(value);
     inputVal = currentArr.join("");
@@ -27,6 +28,9 @@ function sum(value){
 //if valueArr is not populated, then we know it's the first time it's being run. As such, it will push inputVal to valueArr and then get reset
 //if operatorChoice is populated, then we know it's not the first time being run. So it returns the total so it can be chained without the use of the equal operator 
 function inputOperator(operator){
+    if(!inputVal){
+        return;
+    }
     if (!valueArr.length == 1){
         valueArr.push(inputVal);
         inputVal = 0;
@@ -34,8 +38,8 @@ function inputOperator(operator){
     if(operatorChoice != undefined) {
         totalSum()
         inputVal = 0;
-
     }
+   
     currentArr = [];
     operatorChoice = operator;
     return operatorChoice;
@@ -43,6 +47,9 @@ function inputOperator(operator){
 //takes both the strings from the valueArr array and retuns a floating point number;
 //once total has been defined, sets the valueArr equal only the total so that it can be chained
 function totalSum(){
+    if(operatorChoice == undefined){
+        return;
+    }
     valueArr.push(inputVal)
     sum1 = parseFloat(valueArr[0]);
     sum2 = parseFloat(valueArr[1]);
@@ -53,7 +60,7 @@ function totalSum(){
     }else if (operatorChoice == "-"){
         total = (sum1 - sum2);
     }else if(operatorChoice == "/"){
-        if(sum1 == "0" || sum2 == "0"){
+        if(sum2 == "0"){
             alert("Are you tryin to kill me??")
         } else{
             total = (sum1 / sum2);
@@ -64,10 +71,10 @@ function totalSum(){
     valueArr[0] = total;
     valueArr.length = 1;
     operatorChoice = "";
-
     return total;
     }
 
+//removes the last digit
 function backButton(){
     currentArr = currentArr.slice(0,-1);
     inputVal = inputVal.slice(0,-1);
@@ -92,10 +99,3 @@ function lightSwitchOff(){
     lightSwitch--;
 
 }
-
-/* To Do:
-
-:: Keyboard Shortcuts
-
-
-*/
